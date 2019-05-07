96356196a5cdb25cd89a96611cc4e01da9bf2c7d
carlostojal <carlos.tojal@hotmail.com>
2019-05-07 16:28:45 +0100

    Adição de funções

--- M7 - Tratamento de Ficheiros/Leitura_Csv.c
+++ M7 - Tratamento de Ficheiros/Leitura_Csv.c
@@ -6,16 +6,22 @@ typedef struct linhas {
     int altura;
     int kg;
     char telef[20];
+    struct linhas *prox;
 }linha;
 
 int menu();
 void ler_dados(linha *novo);
 void escrever_ficheiro(linha *novo);
+void carregar_dados(linha *lista);
+void adicionar(linha *novo,linha *lista);
+void listar(linha *lista);
 
 int main()
 {
     int opt;
     linha *temp = (linha*) malloc(sizeof(linha));
+    linha *lista = (linha*) malloc(sizeof(linha));
+    lista->prox = NULL;
     do{
         opt=menu();
         switch(opt)
@@ -23,6 +29,10 @@ int main()
             case 1:
                 ler_dados(temp);
                 escrever_ficheiro(temp);
+                break;
+            case 2:
+                carregar_dados(lista);
+                listar(lista);
                 break;
         }
     }while(opt);
@@ -35,10 +45,11 @@ int menu()
     do{
         printf("\n** Leitura_Csv **\n\n");
         printf("1. Adicionar leitura\n");
+        printf("2. Listar\n");
         printf("0. Sair\n\n");
         printf("Opcao: ");
         scanf("%d",&opt);
-    }while(opt<0||opt>1);
+    }while(opt<0||opt>2);
     return opt;
 }
 
@@ -64,4 +75,37 @@ void escrever_ficheiro(linha *novo)
     fp = fopen("dados.csv","a");
     fprintf(fp,"%s;%d;%d;%s\n",novo->nome,novo->altura,novo->kg,novo->telef);
     fclose(fp);
+}
+
+void carregar_dados(linha *lista)
+{
+    FILE *fp;
+    linha *novo = (linha*) malloc(sizeof(linha));
+    novo->prox = NULL;//o prox do novo é o velho
+    fp = fopen("dados.csv","r");
+    while(!feof(fp))
+    {
+        fscanf(fp,"%s;%d;%d;%s",novo->nome,&novo->altura,&novo->kg,novo->telef);
+        adicionar(novo,lista);
+    }
+    fclose(fp);
+}
+
+void adicionar(linha *novo,linha *lista)
+{
+    linha *p = (linha*) malloc(sizeof(lista));
+    if(lista->prox == NULL) lista->prox = novo;
+    else
+    {
+        for(p=lista->prox;p->prox!=NULL;p=p->prox) {}
+        p->prox = novo;
+        lista = p;
+    }
+}
+
+void listar(linha *lista)
+{
+    linha *p = (linha*) malloc(sizeof(linha));
+    for(p=lista->prox;p->prox!=NULL;p=p->prox) {}
+        printf("\nNome: Luisinho Fortnitero\nAltura: %d\nPeso: %d\nTelefone: %s\n\n",p->nome,p->altura,p->kg,p->telef);
 }
