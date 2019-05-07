cfdaf8b8688f0601baa51a234a9376d5e5b39fa3
carlostojal <carlos.tojal@hotmail.com>
2019-05-07 15:08:29 +0100

    Adição de código relacionado com escrita em ficheiros realizado na aula

---
+++ M7 - Tratamento de Ficheiros/Leitura_Csv.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linhas {
+    char nome[20];
+    int altura;
+    int kg;
+    char telef[20];
+}linha;
+
+int menu();
+void ler_dados(linha *novo);
+void escrever_ficheiro(linha *novo);
+
+int main()
+{
+    int opt;
+    linha *temp = (linha*) malloc(sizeof(linha));
+    do{
+        opt=menu();
+        switch(opt)
+        {
+            case 1:
+                ler_dados(temp);
+                escrever_ficheiro(temp);
+                break;
+        }
+    }while(opt);
+    return 0;
+}
+
+int menu()
+{
+    int opt;
+    do{
+        printf("\n** Leitura_Csv **\n\n");
+        printf("1. Adicionar leitura\n");
+        printf("0. Sair\n\n");
+        printf("Opcao: ");
+        scanf("%d",&opt);
+    }while(opt<0||opt>1);
+    return opt;
+}
+
+void ler_dados(linha *novo)
+{
+    printf("\nNome: ");
+    fflush(stdin);
+    //gets(novo->nome);
+    scanf("%s",novo->nome);
+    printf("Altura: ");
+    scanf("%d",&novo->altura);
+    printf("Peso: ");
+    scanf("%d",&novo->kg);
+    printf("Telefone: ");
+    fflush(stdin);
+    //gets(novo->telef);
+    scanf("%s",novo->telef);
+}
+
+void escrever_ficheiro(linha *novo)
+{
+    FILE *fp;
+    fp = fopen("dados.txt","a");
+    fprintf(fp,"%s %d %d %s\n",novo->nome,novo->altura,novo->kg,novo->telef);
+    fclose(fp);
+}
