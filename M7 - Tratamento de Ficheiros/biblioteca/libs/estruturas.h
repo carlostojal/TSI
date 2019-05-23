c3c261080d280b42b360776e5f4ab3ea8ac1f320
Carlos Tojal <36445659+carlostojal@users.noreply.github.com>
2019-05-23 11:13:39 +0100

    Criação das estruturas de dados.

---
+++ M7 - Tratamento de Ficheiros/biblioteca/libs/estruturas.h
@@ -0,0 +1,32 @@
+//Carlos Tojal, nº5, 1ºTSI
+//libs/estruturas.h
+
+#include <stdio.h>
+
+typedef struct Datas {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct Alunos {
+    int num_proc;
+    char nome[30];
+    int ano;
+    char turma[20];
+    char telemovel[20];
+} Aluno;
+
+typedef struct Manuais {
+    char isbn[20];
+    char titulo[20];
+    char disciplina[20];
+} Manual;
+
+typedef struct Emprestimos {
+    int id;
+    int id_aluno;
+    char id_manual[20];
+    Data data_levantamento;
+    float valor;
+} Emprestimo;
