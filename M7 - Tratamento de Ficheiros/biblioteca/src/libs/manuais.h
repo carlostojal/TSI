a1ec032d5159e0f67ae09d4563cab4839269d477
Carlos Cabaço Tojal <a25772>
2019-05-23 15:11:27 +0100

    Adição de bibliotecas ao código main.

---
+++ M7 - Tratamento de Ficheiros/biblioteca/src/libs/manuais.h
@@ -0,0 +1,18 @@
+//Carlos Tojal, nº5, 1ºTSI
+//https://www.github.com/carlostojal/TSI/tree/master/M7 - Tratamento de Ficheiros/biblioteca/src/libs/manuais.h
+
+//Contém as funções de gestão dos manuais
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void adicionar_manual(Manual *lista)
+{
+    Manual *novo;
+    printf("\n** Adicionar Manual**\n\n");
+    printf("ISBN: ");
+    scanf("%s",novo->isbn);
+    printf("Titulo: ");
+    scanf("%s",novo->titulo);
+    printf("Disciplina: ")
+}
