d64f2145b10391deb483e4357c3fd5b32cd67512
carlostojal <carlos.tojal@hotmail.com>
2019-06-05 22:22:41 +0100

    Criação da classe dos empréstimos.

---
+++ M7 - Tratamento de Ficheiros/BibliotecaCPP/include/Emprestimo.h
@@ -0,0 +1,28 @@
+#ifndef EMPRESTIMO_H
+#define EMPRESTIMO_H
+
+
+class Emprestimo
+{
+    public:
+        typedef struct EstEmprestimo {
+            int id;
+            int id_aluno;
+            int id_manual;
+            float valor;
+            struct EstEmprestimo *prox;
+            struct EstEmprestimo *ant;
+        } EstEmprestimo;
+
+        Emprestimo();
+        void AdicionarEmprestimo(Emprestimo::EstEmprestimo *lista);
+        void LimparEmprestimos(Emprestimo::EstEmprestimo *lista);
+        void CarregarEmprestimos(Emprestimo::EstEmprestimo *lista);
+        void ListarEmprestimo(Emprestimo::EstEmprestimo *emprestimo);
+
+    protected:
+
+    private:
+};
+
+#endif // EMPRESTIMO_H
