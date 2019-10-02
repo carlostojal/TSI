823cc7f7055717cd030aa7af2eeda8bf8071844c
Carlos Cabaço Tojal <a25772>
2019-10-02 18:12:41 +0100

    Adição da previsão para 5 dias de 3 em 3 horas.

---
+++ Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/forecast.js
@@ -0,0 +1,33 @@
+
+//
+// Copyright (c) Carlos Tojal (carlostojal)
+// Foz do Arelho
+// scripts/forecast.js 
+// 
+
+// Código responsável por obter informação meteorológica da API do serviço OpenWeatherMap por 5 dias, de 3 em 3 horas
+
+var forecastlink = "https://api.openweathermap.org/data/2.5/forecast?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
+
+function getForecast() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", forecastlink, false);
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function getForecastDate(forecast) {
+    return forecast.dt_txt;
+}
+
+function getForecastStatus(forecast) {
+    return forecast.weather[0].description;
+}
+
+function getForecastMaxTemp(forecast) {
+    return forecast.main.temp_max;
+}
+
+function getForecastMinTemp(forecast) {
+    return forecast.main.temp_min;
+}
--- Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
+++ Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
@@ -7,23 +7,23 @@
 
 // Código responsável por obter informação meteorológica da API do serviço OpenWeatherMap
 
-var forecastlink = "https://api.openweathermap.org/data/2.5/weather?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
+var weatherlink = "https://api.openweathermap.org/data/2.5/weather?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
 
-function getStatus(forecast) {
-    return forecast.weather[0].description;
-}
-
-function getTemperature(forecast) {
-    return forecast.main.temp;
-}
-
-function getWind(forecast) {
-    return forecast.wind.speed;
-}
-
-function getForecast() {
+function getWeather() {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", forecastlink, false);
+    xmlHttp.open("GET", weatherlink, false);
     xmlHttp.send(null);
-    return JSON.parse(xmlHttp.responseText); // a função retorna a resposta da API convertida para um objeto JavaScript
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function getStatus(weather) {
+    return weather.weather[0].description;
+}
+
+function getTemperature(weather) {
+    return weather.main.temp;
+}
+
+function getWind(weather) {
+    return weather.wind.speed;
 }
