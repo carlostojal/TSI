8366cd386625820f4660cc73212286b72ea14929
Carlos Tojal <36445659+carlostojal@users.noreply.github.com>
2019-10-03 09:36:21 +0100

    Add files via upload
    Adição de projeto.

---
+++ Programação e Sistemas de Informação/M8 - Conceitos Avançados de Programação/Exemplo0/Variaveis.java
@@ -0,0 +1,26 @@
+public class Variaveis
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * COnstrutor para objetos da classe Variaveis
+     */
+    public Variaveis()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Exemplo de método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   exemplo de um parâmetro de método
+     * @return     a soma de x com y 
+     */
+    public int sampleMethod(int y)
+    {
+        // ponha seu código aqui
+        return x + y;
+    }
+}
