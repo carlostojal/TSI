812bc1de76485467a7758a2faf50a0eb3e7e0a9c
Carlos Tojal <carlos.tojal@hotmail.com>
2019-10-03 23:24:49 +0100

    Comentários.

--- Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/forecast.js
+++ Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/forecast.js
@@ -5,10 +5,12 @@
 // scripts/forecast.js 
 // 
 
-// Código responsável por obter informação meteorológica da API do serviço OpenWeatherMap por 5 dias, de 3 em 3 horas
+// Código responsável por obter previsão meteorológica da API do serviço OpenWeatherMap
 
+// Link da API
 var forecastlink = "https://api.openweathermap.org/data/2.5/forecast?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
 
+// Função que obtém a previsão em forma de objeto a partir do JSON retornado pela REST API
 function getForecast() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", forecastlink, false);
@@ -16,26 +18,32 @@ function getForecast() {
     return JSON.parse(xmlHttp.responseText);
 }
 
+// Função que obtém a data da previsão em forma de texto
 function getForecastStringDate(forecast) {
     return forecast.dt_txt;
 }
 
+// Função que obtém a data
 function getForecastDate(forecast) {
     return Date.parse(forecast.dt_txt);
 }
 
+// Função que obtém a descrição do estado meteorológico da previsão (céu limpo, nublado, etc.)
 function getForecastStatus(forecast) {
     return forecast.weather[0].description;
 }
 
+// Função que obtém a temperatura da previsão
 function getForecastTemp(forecast) {
     return forecast.main.temp;
 }
 
+// Função que obtém a temperatura máxima da previsão
 function getForecastMaxTemp(forecast) {
     return forecast.main.temp_max;
 }
 
+// Função que obtém a temperatura mínima da previsão
 function getForecastMinTemp(forecast) {
     return forecast.main.temp_min;
 }
--- Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
+++ Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
@@ -5,10 +5,12 @@
 // scripts/weather.js 
 // 
 
-// Código responsável por obter informação meteorológica da API do serviço OpenWeatherMap
+// Código responsável por obter meteorologia atual da API do serviço OpenWeatherMap
 
+// Link da API
 var weatherlink = "https://api.openweathermap.org/data/2.5/weather?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
 
+// Função que obtém o objeto a partir do JSON retornado pela REST API
 function getWeather() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", weatherlink, false);
@@ -16,14 +18,17 @@ function getWeather() {
     return JSON.parse(xmlHttp.responseText);
 }
 
+// Função que obtém o texto que descreve como está o clima (céu limpo, nublado, etc.)
 function getStatus(weather) {
     return weather.weather[0].description;
 }
 
+// Função que obtém a temperatura
 function getTemperature(weather) {
     return weather.main.temp;
 }
 
+// Função que obtém a velocidade do vento
 function getWind(weather) {
     return weather.wind.speed;
 }
