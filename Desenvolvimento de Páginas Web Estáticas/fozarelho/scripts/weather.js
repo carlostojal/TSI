d1c00e0d711abccae0a3e9b33cf70c6c08b9d699
Carlos Tojal <carlos.tojal@hotmail.com>
2019-10-12 18:58:35 +0100

    Agora a meteorologia atual é apresentada em inglês na página inicial da versão inglesa.

--- Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
+++ Desenvolvimento de Páginas Web Estáticas/fozarelho/scripts/weather.js
@@ -10,11 +10,20 @@
 
 // Link da API
 var weatherlink = "https://api.openweathermap.org/data/2.5/weather?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&lang=pt&units=metric";
+var weatherlink_en = "https://api.openweathermap.org/data/2.5/weather?apikey=c9f1d2a6eaec5f917473c187547ba288&id=2267937&units=metric";
 
 // Função que obtém o objeto a partir do JSON retornado pela REST API
 function getWeather() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", weatherlink, false);
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+// Função que obtém o objeto a partir do JSON retornado pela REST API, em inglês
+function getWeatherEn() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", weatherlink_en, false);
     xmlHttp.send(null);
     return JSON.parse(xmlHttp.responseText);
 }
