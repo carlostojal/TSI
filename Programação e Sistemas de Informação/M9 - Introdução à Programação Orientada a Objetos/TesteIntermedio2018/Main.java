450e7ace337ab3e00530f12013c2d59e987a64c7
Carlos Tojal <carlos.tojal@hotmail.com>
2019-10-17 09:47:46 +0100

    Conclusão do teste intermédio do ano anterior.

---
+++ Programação e Sistemas de Informação/M9 - Introdução à Programação Orientada a Objetos/TesteIntermedio2018/Main.java
@@ -0,0 +1,53 @@
+
+/*
+* Copyright (c) Carlos Tojal 2019
+* TesteIntermedio2018
+* Main.java
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    static Almoco[] almocos = new Almoco[4];
+
+    public static void main(String[] args) {
+        int opt;
+        almocos[0] = new Almoco("Sandes de presunto/leitão/salmão", 3f);
+        almocos[1] = new Almoco("Salada Multicor", 2.5f);
+        almocos[2] = new Almoco("Sopa", 1.5f);
+        almocos[3] = new Almoco("Tudo", 6f);
+
+        opt = menu();
+
+        switch(opt) {
+            case 1:
+                System.out.println("\n" + almocos[0].getDescricao() + " - " + almocos[0].getPreco());
+                break;
+            case 2:
+                System.out.println("\n" + almocos[1].getDescricao() + " - " + almocos[1].getPreco());
+                break;
+            case 3:
+                System.out.println("\n" + almocos[2].getDescricao() + " - " + almocos[2].getPreco());
+                break;
+            case 4:
+                System.out.println("\n" + almocos[3].getDescricao() + " - " + almocos[3].getPreco());
+                break;
+        }
+    }
+
+    public static int menu() {
+        Scanner scanner = new Scanner(System.in);
+        int opt;
+        do {
+            System.out.println("\n** Almoços **\n");
+            for(int i = 0; i < 4; i++) {
+                System.out.println((i + 1) + " - " + almocos[i].getDescricao());
+            }
+            System.out.print("Opção: ");
+            opt = scanner.nextInt();
+            if(opt < 1 || opt > 4)
+                System.out.println("Erro: opção inválida.");
+        }while(opt < 1 || opt > 4);
+        return opt;
+    }
+}
