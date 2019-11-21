3e228209f7a663577327521f487eb9f8ed0adaa8
Carlos Tojal <carlos.tojal@hotmail.com>
2019-11-21 22:35:31 +0000

    Conclusão da classe Robot.

--- Programação e Sistemas de Informação/M10 - Programação Orientada a Objetos/Avaliacao/Robot.java
+++ Programação e Sistemas de Informação/M10 - Programação Orientada a Objetos/Avaliacao/Robot.java
@@ -13,6 +13,8 @@ public class Robot {
     private double temperature;
     private byte status;
     private int power;
+    private int maxSpeed;
+    private int currentSpeed;
 
     // Getters e setters
     public String getName() {
@@ -27,12 +29,12 @@ public class Robot {
         return temperature;
     }
 
-    public byte getStatus() {
-        return status;
-    }
-
     public int getPower() {
         return power;
+    }
+
+    private int getMaxSpeed() {
+        return maxSpeed;
     }
 
     public void setName(String name) {
@@ -47,12 +49,12 @@ public class Robot {
         this.temperature = temperature;
     }
 
-    public void setStatus(byte status) {
-        this.status = status;
-    }
-    
     public void setPower(int power) {
         this.power = power;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
     }
 
     // Construtores
@@ -76,11 +78,69 @@ public class Robot {
         return "Name: " + this.getName() + "\n" +
         "Speed: " + this.getSpeed() + "\n" +
         "Temperature: " + this.getTemperature() + "\n" +
-        "Status: " + this.getStatus() + "\n" +
+        "Status: " + this.status + "\n" +
         "Power: " + this.getPower() + "%";
     }
 
     public void show() {
         System.out.println(this.toString());
+    }
+
+    public void stop() {
+        this.currentSpeed = 0;
+        if(this.temperature - 10 >= 0)
+            this.temperature -= 10; 
+        this.status = 4;
+    }
+
+    public void speedUp(int speed) {
+        if(this.currentSpeed + speed <= this.maxSpeed) {
+            this.currentSpeed += speed;
+            this.temperature += (speed * 0.5);
+            this.status = 2;
+        }
+    }
+
+    public void speedUp() {
+        if(this.currentSpeed + (this.currentSpeed * 0.1) <= this.maxSpeed) {
+            double speed = this.currentSpeed * 0.1;
+            this.currentSpeed += speed;
+            this.temperature += (speed * 0.5);
+            this.status = 2;
+        }
+    }
+    
+    public void speedDown(int speed) {
+        if(this.currentSpeed - speed >= 0) {
+            this.currentSpeed -= speed;
+            this.temperature -= (speed * 0.5);
+            if(this.currentSpeed == 0)
+                this.status = 4;
+        }
+    }
+
+    public void breaking() {
+        this.currentSpeed -= (this.currentSpeed * 0.1);
+        if(this.currentSpeed <= 0) {
+            this.currentSpeed = 0;
+            this.status = 4;
+        }
+    }
+
+    public void standBy() {
+        this.currentSpeed = 0;
+        this.temperature = 0.0;
+        this.status = 1;
+    }
+
+    public void exploring() {
+        if(this.status == 4) 
+            this.status = 3;
+        this.temperature += 20.0;
+    }
+
+    public void returning() {
+        if(this.status == 2)
+            this.status = 5;
     }
 }
