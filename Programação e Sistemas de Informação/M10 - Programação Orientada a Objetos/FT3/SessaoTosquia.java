c177c14c0bca72589b95b46d39f2e2030e9299a4
Carlos Tojal <carlos.tojal@hotmail.com>
2019-11-25 16:55:17 +0000

    Criação da classe SessaoTosquia.

---
+++ Programação e Sistemas de Informação/M10 - Programação Orientada a Objetos/FT3/SessaoTosquia.java
@@ -0,0 +1,15 @@
+
+//
+// Carlos Tojal 25-11-2019
+// FT3
+// SessaoTosquia.java
+//
+
+public class SessaoTosquia {
+    
+    public static void main(String[] args) {
+        Tosquiador tosquiador = new Tosquiador();
+        Ovelha ovelha = new Ovelha();
+        tosquiador.tosquiar(ovelha);
+    }
+}
