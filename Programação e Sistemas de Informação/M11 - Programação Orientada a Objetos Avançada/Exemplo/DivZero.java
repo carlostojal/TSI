a0a17d86942b4a8ae14296c96a286a779cb34878
Carlos Tojal <carlos.tojal@hotmail.com>
2019-12-16 17:19:01 +0000

    Começo do módulo 11.

---
+++ Programação e Sistemas de Informação/M11 - Programação Orientada a Objetos Avançada/Exemplo/DivZero.java
@@ -0,0 +1,15 @@
+
+//
+// Copyright (c) Carlos Tojal 2019
+// Exemplo1
+// DivZero.java
+//
+
+public class DivZero {
+    public static void main(String[] args) {
+        int a = 1, b = 0;
+        System.out.println(a/b);
+        System.out.println("Divisão por zero");
+        System.out.println("Fim do programa");
+    }
+}
