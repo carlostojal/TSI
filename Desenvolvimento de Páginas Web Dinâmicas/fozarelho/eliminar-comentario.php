9aca0f479bf668be04c4c59bf4cae17d21f03dea
Carlos Tojal <carlos.tojal@hotmail.com>
2020-03-10 15:34:08 +0000

    Correção de vulnerabilidade.

--- Desenvolvimento de Páginas Web Dinâmicas/fozarelho/eliminar-comentario.php
+++ Desenvolvimento de Páginas Web Dinâmicas/fozarelho/eliminar-comentario.php
@@ -4,7 +4,7 @@
 
     require("connection.php");
     
-    $sql = "SELECT * FROM comentario INNER JOIN utilizador ON comentario.username = utilizador.username WHERE comentario.id = '".$_GET['id']."'";
+    $sql = "SELECT * FROM comentario INNER JOIN utilizador ON comentario.username = utilizador.username WHERE comentario.id = '".$_GET['id']."' AND utilizador.username = '".$_SESSION['username']."'";
     $res = mysqli_query($con, $sql);
 
     if(mysqli_num_rows($res) == 1) {
